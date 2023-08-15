03b5160cb832ee7a04d8569ec0302ca696189be9
shakeel071 <shakeelmohammed474@gmail.com>
2023-08-15 15:26:35 +0530

    changes

--- src/SignupOne.java
+++ src/SignupOne.java
@@ -71,9 +71,15 @@ public class SignupOne  extends JFrame implements ActionListener{
         female.setBackground(Color.WHITE);
         add(female);
 
+        other=new JRadioButton("Other");
+        other.setBounds(600,290,100,30);
+        other.setBackground(Color.WHITE);
+        add(other);
+
         ButtonGroup gendergroup=new ButtonGroup();
         gendergroup.add(male);
         gendergroup.add(female);
+        gendergroup.add(other);
 
 
         JLabel email=new JLabel("Email Address:");
@@ -101,15 +107,11 @@ public class SignupOne  extends JFrame implements ActionListener{
         unmarried.setBackground(Color.WHITE);
         add(unmarried);
 
-         other=new JRadioButton("Other");
-        other.setBounds(630,390,100,30);
-        other.setBackground(Color.WHITE);
-        add(other);
+         
 
         ButtonGroup maritalgroup=new ButtonGroup();
         maritalgroup.add(married);
         maritalgroup.add(unmarried);
-        maritalgroup.add(other);
 
 
 
@@ -181,6 +183,8 @@ public class SignupOne  extends JFrame implements ActionListener{
         gender="Male";
        }else if(female.isSelected()){
         gender="Female";
+       }else if(other.isSelected()){
+        gender="Other";
        }
 
        String email=emailTextField.getText();
@@ -190,8 +194,6 @@ public class SignupOne  extends JFrame implements ActionListener{
         marital="Married";
        }else if(unmarried.isSelected()){
         marital="Unnmarried";
-       }else if(other.isSelected()){
-        marital="Other";
        }
 
        String address=addressTextField.getText();
